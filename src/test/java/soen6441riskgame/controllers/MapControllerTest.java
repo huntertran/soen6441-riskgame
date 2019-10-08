b15255ee8ee70baf69193f7803ae9ea2050b13ec
bharti8800 <bharti8800@gmail.com>
2019-10-08 15:07:02 -0400

    updated unit test for MapControllerTest

--- src/test/java/soen6441riskgame/controllers/MapControllerTest.java
+++ src/test/java/soen6441riskgame/controllers/MapControllerTest.java
@@ -20,23 +20,27 @@ public class MapControllerTest {
 
 	@Parameters
 	public static Collection<Object[]> data() {
-		return Arrays.asList(new Object[][] { { "Asia", "Vietnam", "Laos", "13" },
-				{ "Europe", "Spain", "France", "12" }, { "South Africa", "Guinea", "Sierra Leone", "8" },
-				{ "North America", "Canada", "USA", "11" } });
+		return Arrays.asList(new Object[][] { { "Russia", "Asia", "Vietnam", "Laos", "13", "12" },
+				{ "North America", "Europe", "Spain", "France", "12", "13" }, { "Asia", "South Africa", "Guinea", "Sierra Leone", "8","20" },
+				{ "South Africa", "North America", "Canada", "USA", "11","14" } });
 	}
 
 	private String continent1;
+	private String continent2;
 	private String country1;
 	private String country2;
 	private String continent1_value;
+	private String continent2_value;
 
 	MapController mapController;
-
-	public MapControllerTest(String continent1, String country1, String country2, String continent1_value) {
+	
+	public MapControllerTest(String continent1, String continent2, String country1, String country2, String continent1_value, String continent2_value) {
 		this.continent1 = continent1;
+		this.continent2 = continent2;
 		this.country1 = country1;
 		this.country2 = country2;
 		this.continent1_value = continent1_value;
+		this.continent2_value = continent2_value;
 	}
 
 	/**
@@ -49,7 +53,7 @@ public class MapControllerTest {
 	}
 
 	/**
-	 * This function will run after all test cases have been executed
+	 * This function will run after all test cases have been executed and reset the map.
 	 */
 	@AfterClass
 	public static void doAfterClass() {
@@ -57,6 +61,9 @@ public class MapControllerTest {
 		mapController.resetMap();
 	}
 
+	/**
+	 * This loadMapTest function tests if the map loaded from a file is working properly.
+	 */
 	@Test
 	public void loadMapTest() {
 		// Setup
@@ -78,30 +85,51 @@ public class MapControllerTest {
 	 * // Action mapController.showMap(); }
 	 */
 
+	/**
+	 * Tests add Continent method
+	 * It calls addContinent method and tests whether the continent is created and has the same control value as passed.
+	 */
 	@Test
 	public void addContinentTest() {
 		// Action
 		mapController.addContinent(continent1, continent1_value);
-
+		mapController.addContinent(continent2, continent2_value);
+		
 		// Assert
 		Assert.assertTrue(mapController.isContinentExisted(continent1));
 		Assert.assertEquals(continent1_value,
 				Integer.toString(mapController.getContinentFromName(continent1).getArmy()));
+		Assert.assertTrue(mapController.isContinentExisted(continent2));
+		Assert.assertEquals(continent2_value,
+				Integer.toString(mapController.getContinentFromName(continent2).getArmy()));
 	}
 
+	/**
+	 * Tests remove continent method
+	 * And tests whether the continent has been removed
+	 * And the countries in that continent are now existing without continent
+	 */
 	@Test
 	public void removeContinentTest() {
 		// Setup
 		mapController.addContinent(continent1, continent1_value);
+		mapController.addCountry(country1, continent1);
+		mapController.addCountry(country2, continent1);
 
 		// Action
 		mapController.removeContinent(continent1);
 
 		// Assert
 		Assert.assertNull(mapController.getContinentFromName(continent1));
+		Assert.assertNotEquals(continent1, mapController.getCountryFromName(country1).getContinent());
+		Assert.assertNotEquals(continent1, mapController.getCountryFromName(country2).getContinent());
 
 	}
-
+	
+	/**
+	 * Tests add Country method
+	 * It calls the add country method and tests whether that country has been created.
+	 */
 	@Test
 	public void addCountryTest() {
 		// Setup
@@ -109,24 +137,37 @@ public class MapControllerTest {
 
 		// Action
 		mapController.addCountry(country1, continent1);
+		mapController.addCountry(country2, continent1);
 
 		// Assert
 		Assert.assertTrue(mapController.isCountryExisted(country1));
+		Assert.assertTrue(mapController.isCountryExisted(country2));
 	}
 
+	/**
+	 * Tests remove Country method
+	 * It calls the remove country method and tests whether that country has been removed.
+	 */
 	@Test
 	public void removeCountryTest() {
 		// Setup
 		mapController.addContinent(continent1, continent1_value);
 		mapController.addCountry(country1, continent1);
+		mapController.addCountry(country2, continent1);
 
 		// Action
 		mapController.removeCountry(country1);
+		mapController.removeCountry(country2);
 
 		// Assert
 		Assert.assertFalse(mapController.isCountryExisted(country1));
+		Assert.assertFalse(mapController.isCountryExisted(country2));
 	}
 
+	/**
+	 * Tests add Neighbor method
+	 * It calls the add neighbor method and tests whether the 2 countries are neighbors.
+	 */
 	@Test
 	public void addNeighborTest() {
 		// Setup
@@ -141,6 +182,10 @@ public class MapControllerTest {
 		Assert.assertTrue(mapController.isNeighboringCountries(country1, country2));
 	}
 
+	/**
+	 * Tests edit Continent method
+	 * It adds continent using edit continent method and tests whether that continent exists.
+	 */
 	@Test
 	public void editContinentTest1() {
 		// Action
@@ -149,8 +194,14 @@ public class MapControllerTest {
 
 		// Assert
 		Assert.assertTrue(mapController.isContinentExisted(continent1));
+		Assert.assertEquals(continent1_value,
+				Integer.toString(mapController.getContinentFromName(continent1).getArmy()));
 	}
 
+	/**
+	 * Tests edit Continent method
+	 * It removes continent using edit continent method and tests whether that continent does not exist.
+	 */
 	@Test
 	public void editContinentTest2() {
 		// Action
@@ -161,6 +212,10 @@ public class MapControllerTest {
 		Assert.assertFalse(mapController.isContinentExisted(continent1));
 	}
 
+	/**
+	 * Tests edit Country method
+	 * It adds country using edit country method and tests whether that country exists.
+	 */
 	@Test
 	public void editCountryTest1() {
 		// Setup
@@ -174,6 +229,10 @@ public class MapControllerTest {
 		Assert.assertTrue(mapController.isCountryExisted(country1));
 	}
 
+	/**
+	 * Tests edit Country method
+	 * It removes country using edit country method and tests whether that country does not exist.
+	 */
 	@Test
 	public void editCountryTest2() {
 		// Setup
@@ -187,6 +246,10 @@ public class MapControllerTest {
 		Assert.assertFalse(mapController.isCountryExisted(country1));
 	}
 
+	/**
+	 * Tests remove Neighbor method
+	 * It calls the remove neighbor method and tests whether the 2 countries are not neighbors.
+	 */
 	@Test
 	public void removeNeighborTest() {
 		// Setup
@@ -202,6 +265,10 @@ public class MapControllerTest {
 		Assert.assertFalse(mapController.isNeighboringCountries(country1, country2));
 	}
 
+	/**
+	 * Tests edit Neighbor method
+	 * It adds neighbor using edit neighbor method and tests whether the 2 countries are neighbors.
+	 */
 	@Test
 	public void editNeighborTest1() {
 		// Setup
@@ -217,19 +284,23 @@ public class MapControllerTest {
 		Assert.assertTrue(mapController.isNeighboringCountries(country1, country2));
 	}
 
+	/**
+	 * Tests edit Neighbor method
+	 * It removes neighbor using edit neighbor method and tests whether the 2 countries are not neighbors.
+	 */
 	@Test
 	public void editNeighborTest2() {
 		// Setup
 		mapController.addContinent(continent1, continent1_value);
 		mapController.addCountry(country1, continent1);
 		mapController.addCountry(country2, continent1);
-		String arguments[] = { "-add", country1, country2 };
+		String arguments[] = { "-remove", country1, country2 };
 
 		// Action
 		mapController.editNeighbor(arguments);
 
 		// Assert
-		Assert.assertTrue(mapController.isNeighboringCountries(country1, country2));
+		Assert.assertFalse(mapController.isNeighboringCountries(country1, country2));
 	}
 
 }
