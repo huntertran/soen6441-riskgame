413825b279120708ef3fa9794acb1ba7c19c9cdb
Benjamin Osei Asante <55560613+Benjicals-hub@users.noreply.github.com>
2019-11-01 17:34:05 -0400

    Card

---
+++ src/main/java/soen6441riskgame/enums/Card.java
@@ -0,0 +1,17 @@
+/**
+ * The Enum Card.
+ * This class has cards a player can possibly own.
+ * 
+ * 
+ * @version 1.0.0
+ */
+
+public enum Card {
+
+    /** The Infantry. */
+    Infantry,
+    /** The Cavalry. */
+    Cavalry,
+    /** The Artillery. */
+    Artillery
+}
