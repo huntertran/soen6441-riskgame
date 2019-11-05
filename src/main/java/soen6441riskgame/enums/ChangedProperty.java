a3c7721b364c2f9f57b47a3650e1256e0132736d
Van Tuan Tran <4468525+huntertran@users.noreply.github.com>
2019-11-05 15:45:17 -0500

    javadoc for ChangedProperty

--- src/main/java/soen6441riskgame/enums/ChangedProperty.java
+++ src/main/java/soen6441riskgame/enums/ChangedProperty.java
@@ -1,10 +1,10 @@
 package soen6441riskgame.enums;
 
 /**
- * The Change Property
- * 
+ * Because the observers cannot differentiate what property is changed, this class indicate that
+ * info
  */
 public enum ChangedProperty {
-    CARD,
-    GAME_PHASE
+                             CARD,
+                             GAME_PHASE
 }
