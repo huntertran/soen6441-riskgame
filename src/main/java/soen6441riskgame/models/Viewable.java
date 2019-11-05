42097a0159dfdae729e04645d6191ba3311899b0
Van Tuan Tran <4468525+huntertran@users.noreply.github.com>
2019-11-05 15:37:47 -0500

    javadoc for interface Viewable

--- src/main/java/soen6441riskgame/models/Viewable.java
+++ src/main/java/soen6441riskgame/models/Viewable.java
@@ -4,12 +4,24 @@ package soen6441riskgame.models;
  * indicate an object is printable to console with/without indentation
  */
 public interface Viewable {
+
+    /**
+     * view with a number of indent
+     * @param indent number of indent
+     */
     public void view(int indent);
 
+    /**
+     * view without indent
+     */
     public default void view() {
         view(0);
     }
 
+    /**
+     * print the indent with 4 spaces
+     * @param indent number of indent
+     */
     public default void printIndent(int indent) {
         for (int index = 0; index < indent; index++) {
             System.out.print("    ");
