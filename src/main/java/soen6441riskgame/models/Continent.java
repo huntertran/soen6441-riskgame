602fee588edd29b2b78546429b218030fb1a72f3
Van Tuan Tran <4468525+huntertran@users.noreply.github.com>
2019-11-05 14:09:13 -0500

    javadoc for Continent

--- src/main/java/soen6441riskgame/models/Continent.java
+++ src/main/java/soen6441riskgame/models/Continent.java
@@ -12,12 +12,17 @@ import soen6441riskgame.utils.GraphChecker;
 public class Continent extends Observable implements Viewable {
     private String name;
     private int army;
-    // private Player oldConquerer;
     private ArrayList<Country> countries = new ArrayList<Country>();
 
+    /**
+     * constructor
+     *
+     * @param name name of the continent
+     * @param army the continent value
+     */
     public Continent(String name, int army) {
-        this.setName(name);
-        this.setArmy(army);
+        this.name = name;
+        this.army = army;
     }
 
     /**
@@ -27,28 +32,29 @@ public class Continent extends Observable implements Viewable {
         return GameBoard.getInstance().getGameBoardMap().getContinents().indexOf(this) + 1;
     }
 
+    /**
+     * get continent's army that will be rewarded to player whose conquer this continent
+     *
+     * @return the number of army added to conquerer
+     */
     public int getArmy() {
         return army;
     }
 
-    public void setArmy(int army) {
-        this.army = army;
-    }
-
+    /**
+     * get all the countries inside this continent
+     *
+     * @return the countries inside this continent
+     */
     public ArrayList<Country> getCountries() {
         return countries;
     }
 
-    public void setCountries(ArrayList<Country> countries) {
-        this.countries = countries;
-    }
-
+    /**
+     * get continent name
+     */
     public String getName() {
         return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 
     /**
@@ -74,11 +80,20 @@ public class Continent extends Observable implements Viewable {
         return countryConquerer;
     }
 
+    /**
+     * check if the continent is a mini-connected graph
+     *
+     * @see https://github.com/huntertran/soen6441-riskgame/wiki/Connected-Graph-Validation-Unit-Test
+     * @return false if it's not a mini-connected graph
+     */
     public boolean isContinentConnected() {
         ArrayList<Country> countries = getCountries();
         return GraphChecker.isCountriesConnected(countries);
     }
 
+    /**
+     * print the continent content
+     */
     public void view(int indent) {
         this.viewWithoutCountry();
 
@@ -87,6 +102,9 @@ public class Continent extends Observable implements Viewable {
         }
     }
 
+    /**
+     * print the continent content without printing it's countries
+     */
     public void viewWithoutCountry() {
         String printString = "Continent: %s\t| No.: %s\t| Number of army: %s";
 
