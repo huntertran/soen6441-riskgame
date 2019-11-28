d2308349297f8963456d16fd1170a9c23a07451e
Van Tuan Tran <4468525+huntertran@users.noreply.github.com>
2019-11-28 14:01:51 -0500

    implement #58

--- src/main/java/soen6441riskgame/models/strategies/HumanStrategy.java
+++ src/main/java/soen6441riskgame/models/strategies/HumanStrategy.java
@@ -1,10 +1,16 @@
 package soen6441riskgame.models.strategies;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
+import soen6441riskgame.App;
+import soen6441riskgame.enums.GamePhase;
 import soen6441riskgame.enums.StrategyName;
 import soen6441riskgame.models.Country;
+import soen6441riskgame.models.ModelCommands;
 import soen6441riskgame.models.Player;
+import soen6441riskgame.singleton.GameBoard;
+import soen6441riskgame.utils.ConsolePrinter;
 
 public class HumanStrategy implements Strategy {
     /**
@@ -19,26 +25,34 @@ public class HumanStrategy implements Strategy {
 
     @Override
     public void reinforce(Player player, Country countryToReinforce) {
-        // TODO Auto-generated method stub
-
+        return;
     }
 
     @Override
     public ArrayList<Country> attack(Player player, Country attackingCountry) {
-        // TODO Auto-generated method stub
         return null;
     }
 
     @Override
     public void fortify(Country fromCountry, Country toCountry) {
-        // TODO Auto-generated method stub
-
+        return;
     }
 
     @Override
     public void playTurn(Player player) {
-        // TODO Auto-generated method stub
+        ConsolePrinter.printFormat(GameBoard.getInstance().standardPrintStream,
+                                   "HUMAN PLAYER TURN");
+        Scanner scanner = new Scanner(System.in);
 
+        while (player.isPlaying() && player.getCurrentPhase() != GamePhase.END_OF_GAME) {
+            String command = scanner.nextLine();
+            ModelCommands cmds = new ModelCommands(command);
+            App.jumpToCommand(cmds);
+            ConsolePrinter.printFormat(GameBoard.getInstance().standardPrintStream,
+                                       "ENTER YOUR ACTION: ");
+            command = scanner.nextLine();
+        }
+
+        scanner.close();
     }
-
 }
