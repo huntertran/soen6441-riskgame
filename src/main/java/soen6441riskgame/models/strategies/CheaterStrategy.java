c5ca1cfef8eca8119fbf19f8d8eab50fdc580904
Van Tuan Tran <4468525+huntertran@users.noreply.github.com>
2019-12-02 11:19:23 -0500

    when cheater attack cause other player lost, set in Cheater strategy

--- src/main/java/soen6441riskgame/models/strategies/CheaterStrategy.java
+++ src/main/java/soen6441riskgame/models/strategies/CheaterStrategy.java
@@ -1,5 +1,6 @@
 package soen6441riskgame.models.strategies;
 
+import soen6441riskgame.enums.GamePhase;
 import soen6441riskgame.enums.StrategyName;
 import soen6441riskgame.models.Country;
 import soen6441riskgame.models.Player;
@@ -27,7 +28,7 @@ public class CheaterStrategy implements Strategy {
     }
 
     /**
-     * reinforce
+     * reinforce doubles the number of armies on all its countries
      *
      * @param player             current player
      * @param countryToReinforce country to reinforce
@@ -44,7 +45,7 @@ public class CheaterStrategy implements Strategy {
     }
 
     /**
-     * attack
+     * attack automatically conquers all the neighbors of all its countries
      *
      * @param player           current player
      * @param attackingCountry attack from
@@ -54,15 +55,20 @@ public class CheaterStrategy implements Strategy {
     public ArrayList<Country> attack(Player player, Country attackingCountry) {
         ArrayList<Country> conqueredCountries = player.getConqueredCountries();
 
-        if(conqueredCountries.size() <= 0) {
+        if (conqueredCountries.size() <= 0) {
             attackEnd();
         }
-        
+
         for (Country country : conqueredCountries) {
             ArrayList<Country> neighbors = country.getNeighbors();
             for (Country neighbor : neighbors) {
                 if (neighbor.getConquerer() != player) {
+                    Player originalConquerer = neighbor.getConquerer();
                     neighbor.setConquerer(player);
+
+                    if (originalConquerer.getConqueredCountries().size() == 0) {
+                        originalConquerer.setCurrentPhase(GamePhase.LOST);
+                    }
                 }
             }
         }
@@ -71,7 +77,7 @@ public class CheaterStrategy implements Strategy {
     }
 
     /**
-     * fortify
+     * fortify doubles the number of armies on its countries that have neighbors that belong to
      *
      * @param fromCountry from country
      * @param toCountry   to country
@@ -97,19 +103,18 @@ public class CheaterStrategy implements Strategy {
 
         attack(player, null);
         attackEnd();
-        
-        if( player.isGameEnded() ) {
+
+        if (player.isGameEnded()) {
             player.setEndOfGamePhase();
-        }
-        else {
-        
+        } else {
+
             // the cheater strategy required re-implementation of fortify command
             ArrayList<Country> countriesToFortify = getCountriesToFortify(player);
             for (Country toCountry : countriesToFortify) {
                 Country fromCountry = getFortifyFromCountry(player, toCountry);
                 fortify(fromCountry, toCountry);
             }
-    
+
             fortifyNone();
         }
     }
